0bc1b0faa7cc5f67f0c8fb04cd1cdb7ceaca1d10
Anderson Rocha <32132934+andersondesigner3d@users.noreply.github.com>
2024-08-27 23:51:55 -0300

    feito corte e adicionado efeitos sonoros e visuais do primeiro ataque e pulo

---
+++ Assets/obj_attack1.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obj_attack1 : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void selfDestroy(){
+        Destroy(gameObject);
+    }
+}
--- Assets/scripts/ichigo/player.cs
+++ Assets/scripts/ichigo/player.cs
@@ -26,7 +26,15 @@ public class player : MonoBehaviour
     public float deadZone = 0.1f;
     public float jumpingPower;
     public bool velocidadeY;
-
+    [Header ("Atack1")]
+    public bool attacking;
+    [Header ("Visual FX")]
+    public bool canDust = true;
+    public GameObject attack1_fx;
+    public GameObject dust_jump_fx;
+    [Header ("Audio")]
+    public AudioSource audioSource;
+    public AudioClip[] audioClip;
 
     void Start()
     {
@@ -38,13 +46,13 @@ public class player : MonoBehaviour
     void Update()
     {        
         //print("horizontalMoviment: "+horizontalMoviment);
-        //print("rb.velocity.x = "+rb.velocity.x);
+        //print("rb.velocity.y = "+rb.velocity.y);
         //print("verticalMoviment = "+verticalMoviment);
 
         touchingGround = IsGrounded();
         touchingWall = canMove();
         verificaVelocidadeY();
-        if(IsGrounded()){
+        if(touchingGround){
             jumpping = false;
         }
 
@@ -55,69 +63,81 @@ public class player : MonoBehaviour
             Flip();
         }        
         
-
         //moviment
-        if(!touchingWall){
-            rb.velocity = new Vector2(horizontalMoviment * moveSpeed, rb.velocity.y);
-            
-        }        
+        if(attacking){
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        } else {
+            if(!touchingWall && !attacking){
+                rb.velocity = new Vector2(horizontalMoviment * moveSpeed, rb.velocity.y);
+            }
+        }
+
+        fixAnimationBugs();
+        
         
     }
 
-    private void FixedUpdate() {
-
-        if(touchingGround){            
-            if(horizontalMoviment == 0 || touchingWall){
-                anim.SetBool("parado",true);
-                anim.SetBool("correndo",false);
-                anim.SetBool("pulando-subindo",false);
-                anim.SetBool("pulando-caindo",false);
-                anim.SetBool("sofrendo-dano",false);
-                anim.SetBool("morto",false);
-                anim.SetBool("ataque1",false);
-                anim.SetBool("especial",false);
-            } else if(horizontalMoviment != 0 && !touchingWall && rb.velocity.x != 0) {
-                anim.SetBool("parado",false);
-                anim.SetBool("correndo",true);
-                anim.SetBool("pulando-subindo",false);
-                anim.SetBool("pulando-caindo",false);
-                anim.SetBool("sofrendo-dano",false);
-                anim.SetBool("morto",false);
-                anim.SetBool("ataque1",false);
-                anim.SetBool("especial",false);
-            }
+    private void fixAnimationBugs(){
+        //fiz animations bugs
+        if(touchingGround){
+            anim.SetBool("pulando-subindo",false);
+            anim.SetBool("pulando-caindo",false);
         } else {
-            if(rb.velocity.y >= 0){
-                anim.SetBool("parado",false);
-                anim.SetBool("correndo",false);
-                anim.SetBool("pulando-subindo",true);
-                anim.SetBool("pulando-caindo",false);
-                anim.SetBool("sofrendo-dano",false);
-                anim.SetBool("morto",false);
-                anim.SetBool("ataque1",false);
-                anim.SetBool("especial",false);
-            } else {
-                anim.SetBool("parado",false);
-                anim.SetBool("correndo",false);
-                anim.SetBool("pulando-subindo",false);
-                anim.SetBool("pulando-caindo",true);
-                anim.SetBool("sofrendo-dano",false);
-                anim.SetBool("morto",false);
-                anim.SetBool("ataque1",false);
-                anim.SetBool("especial",false);
-            }
+            anim.SetBool("parado",false);
+            anim.SetBool("correndo",false);
         }
     }
 
-    public void Move(InputAction.CallbackContext context){
+    private void FixedUpdate() {
+        //animations control
+        if(!attacking){
+            if(touchingGround){            
+                if(horizontalMoviment == 0 || touchingWall){
+                    anim.SetBool("parado",true);
+                    anim.SetBool("correndo",false);
+                    anim.SetBool("pulando-subindo",false);
+                    anim.SetBool("pulando-caindo",false);
+                    anim.SetBool("sofrendo-dano",false);
+                    anim.SetBool("morto",false);
+                } else if(horizontalMoviment != 0 && !touchingWall && rb.velocity.x != 0) {
+                    anim.SetBool("parado",false);
+                    anim.SetBool("correndo",true);
+                    anim.SetBool("pulando-subindo",false);
+                    anim.SetBool("pulando-caindo",false);
+                    anim.SetBool("sofrendo-dano",false);
+                    anim.SetBool("morto",false);
+                }
+            } else {
+                if(rb.velocity.y >= 0){
+                    anim.SetBool("parado",false);
+                    anim.SetBool("correndo",false);
+                    anim.SetBool("pulando-subindo",true);
+                    anim.SetBool("pulando-caindo",false);
+                    anim.SetBool("sofrendo-dano",false);
+                    anim.SetBool("morto",false);
+                } else {
+                    anim.SetBool("parado",false);
+                    anim.SetBool("correndo",false);
+                    anim.SetBool("pulando-subindo",false);
+                    anim.SetBool("pulando-caindo",true);
+                    anim.SetBool("sofrendo-dano",false);
+                    anim.SetBool("morto",false);
+                }
+            }
+        }
+
+        //verify dust on falling
+        dustInGround();
         
+    }
+
+    public void Move(InputAction.CallbackContext context){        
         horizontalMoviment = Mathf.RoundToInt(context.ReadValue<Vector2>().x);
         verticalMoviment = Mathf.RoundToInt(context.ReadValue<Vector2>().y);
 
         if(context.canceled){
             StartCoroutine(delayControl());
         }
-
     }
 
     IEnumerator delayControl(){
@@ -128,13 +148,44 @@ public class player : MonoBehaviour
     }
 
     public void Jump(InputAction.CallbackContext context){
+        if(attacking)
+            return;
+
         if(context.performed && IsGrounded()){
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            playSoundJump(0);
+            if(canDust && !jumpping){
+                Instantiate(dust_jump_fx, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+                canDust = false;
+                jumpping = true;
+                StartCoroutine(nowCanDust());
+            }
         }
 
         if(context.canceled && rb.velocity.y > 0f){
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
+
+        anim.SetBool("parado",false);
+        anim.SetBool("pulando-subindo",true);
+
+        
+        
+        
+    }
+
+    private void dustInGround(){
+        if(rb.velocity.y < -0.5f && canDust && !jumpping && touchingGround){
+            playSoundLanding(1);
+            Instantiate(dust_jump_fx, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+            canDust = false;
+            StartCoroutine(nowCanDust());
+        }
+    }
+
+    IEnumerator nowCanDust(){
+        yield return new WaitForSeconds(0.5f);        
+        canDust = true;    
     }
 
     private bool IsGrounded(){
@@ -160,6 +211,8 @@ public class player : MonoBehaviour
     }
 
     private void Flip(){
+        if(attacking)
+            return;
         isFacingRight = !isFacingRight;
         Vector3 localScale = transform.localScale;
         localScale.x *= -1f;
@@ -167,7 +220,35 @@ public class player : MonoBehaviour
     }
 
     public void Sword(InputAction.CallbackContext context){
-        
+        if(attacking || !touchingGround)
+            return;
+        attacking = true;
+        anim.SetTrigger("ataque1");        
+        anim.SetBool("parado",false);
+        anim.SetBool("correndo",false);
+        anim.SetBool("pulando-subindo",false);
+        anim.SetBool("pulando-caindo",false);
+        anim.SetBool("sofrendo-dano",false);
+        anim.SetBool("morto",false);        
+    }
+
+    public void createAttack1_fx(){
+        var temporary_attack1_fx = Instantiate(attack1_fx, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+        temporary_attack1_fx.transform.parent = gameObject.transform;
+
+        // Ajusta a direção do efeito de acordo com a direção do player
+        Vector3 fxScale = temporary_attack1_fx.transform.localScale;
+        fxScale.x *= isFacingRight ? 1f : -1f;
+        temporary_attack1_fx.transform.localScale = fxScale;
+
+        temporary_attack1_fx.transform.parent = null;
+    }
+
+    public void fimAtaque(){
+        anim.ResetTrigger("ataque1");
+        anim.SetBool("parado",true);
+        attacking = false;
+        print(attacking);
     }
 
     void verificaVelocidadeY(){
@@ -177,4 +258,37 @@ public class player : MonoBehaviour
             velocidadeY = false;
         }
     }
+
+    // private void OnTriggerEnter2D(Collider2D other) {
+    //     if(other.gameObject.CompareTag("ground")){
+
+    //     }
+    // }
+
+    //================== SOUNDs =====================
+    public void playSoundJump(int position){
+        this.audioSource.enabled = true;
+        this.audioSource.clip = audioClip[position];
+        this.audioSource.PlayOneShot(this.audioSource.clip);
+    }
+
+    public void playSoundLanding(int position){
+        this.audioSource.enabled = true;
+        this.audioSource.clip = audioClip[position];
+        this.audioSource.PlayOneShot(this.audioSource.clip);
+    }
+
+    public void playSoundSwordAttack1(int position){
+        this.audioSource.enabled = true;
+        this.audioSource.clip = audioClip[position];
+        this.audioSource.PlayOneShot(this.audioSource.clip);
+    }
+
+    public void playSoundIchigoVoice1(int position){
+        this.audioSource.enabled = true;
+        this.audioSource.clip = audioClip[position];
+        this.audioSource.PlayOneShot(this.audioSource.clip);
+    }
+
+    //================= END SOUNDS ===================
 }
